f35435835f3e42ffd9cc323e9f60ae71d23cab7a
Daksh Miglani <hello@dak.sh>
2017-12-11 16:06:44 +0530

    db start done!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import routes from './routes'
 import middleware from './middlware'
+import startDB from './db/start'
 const app = express()
+
+// start the database boy.
+startDB()
 
 // middleware
 middleware(app)
