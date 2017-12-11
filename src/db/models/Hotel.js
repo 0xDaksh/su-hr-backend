4eb2667f0e20fe07f27e33e296bc2cd8cb3c4544
Daksh Miglani <hello@dak.sh>
2017-12-12 00:04:26 +0530

    Added shortid for hotels

--- src/db/models/Hotel.js
+++ src/db/models/Hotel.js
@@ -1,11 +1,17 @@
 import mongoose, {Schema} from 'mongoose'
-
+const shortid = require('shortid')
 const HotelSchema = new Schema({
 	name: String,
 	address: String,
 	users: [{type: Schema.Types.ObjectId, ref: 'User'}],
 	averageRating: Number,
-	image: String
+	image: String,
+	id: {
+		type: String,
+		unique: true,
+		required: true,
+		default: shortid.generate
+	}
 })
 
 export default mongoose.model('Hotel', HotelSchema)
