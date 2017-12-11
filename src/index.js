56a915e4484788e3fef3edc1b579cb556205424b
Daksh Miglani <hello@dak.sh>
2017-12-11 15:59:26 +0530

    added other config

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import routes from './routes'
+import middleware from './middlware'
+const app = express()
+
+// middleware
+middleware(app)
+
+// routes
+app.use('/', routes)
+
+// export it
+export default app
