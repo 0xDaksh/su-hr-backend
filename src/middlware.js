9dd787bf8f69c14c0e81a9e1dcc32a9cdd140e7a
Daksh Miglani <hello@dak.sh>
2017-12-11 18:27:39 +0530

    add cors

--- src/middlware.js
+++ src/middlware.js
@@ -1,6 +1,7 @@
 import expressSession from 'express-session'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 const sess = new expressSession({
 	secret: "yoyo-boy-showttime" + Math.random().toString(36).substr(7),
 	resave: true,
@@ -8,6 +9,7 @@ const sess = new expressSession({
 })
 
 export default (app) => {
+	app.use(app.use(cors({ credentials: true })))
 	app.use(cookieParser())
 	app.use(bodyParser.urlencoded({extended: false}))
 	app.use(sess)
