3ac2d9edef1d37fb757be5c6b613b075b541eee7
Daksh Miglani <hello@dak.sh>
2017-12-12 18:54:58 +0530

    yo boy it's showtime

--- src/middlware.js
+++ src/middlware.js
@@ -16,6 +16,6 @@ export default (app, io) => {
 	io.use(socketSession(session, {
 		autoSave: true
 	}))
-	app.use(cors({origin: 'http://localhost:8080', credentials: true}))
+	app.use(cors({origin: 'https://dak.sh/stayuncle', credentials: true}))
 	app.use(bodyParser.json())
 }
